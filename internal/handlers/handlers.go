be9c6f74819b934ac328866f17d0e568e01baa0a
VladR00 <nazvaniepoch@gmail.com>
2025-05-23 22:01:25 +0300

    check in HPOSTQuote is http.MethodPost also getting error if imposible decoding data. Also add package storage with 2 map and struct

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,10 +1,12 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	cors "webrestapi/internal/cors"
+	storage "webrestapi/internal/storage"
 )
 
 func HandlerGETRandomQuote(w http.ResponseWriter, r *http.Request) {
@@ -16,10 +18,10 @@ func HandlerQuote(w http.ResponseWriter, r *http.Request) {
 	cors.EnableCors(w)
 
 	switch r.Method {
-	case "GET":
-		HGETQuote(w, r)
 	case "POST":
 		HPOSTQuote(w, r)
+	case "GET":
+		HGETQuote(w, r)
 	case "DELETE":
 		HDELETEQuote(w, r)
 	}
@@ -28,7 +30,24 @@ func HandlerQuote(w http.ResponseWriter, r *http.Request) {
 
 func HPOSTQuote(w http.ResponseWriter, r *http.Request) {
 	cors.EnableCors(w)
+	response := map[string]string{"error": "Only POST method allowed"}
 
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+
+	data := storage.Quotes{}
+
+	if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		response = map[string]string{"error": "Bad request"}
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(response)
+		return
+	}
 }
 
 func HGETQuote(w http.ResponseWriter, r *http.Request) {
--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,1 +1,12 @@
-//map
+package storage
+
+var (
+	MapByID     = map[int64]Quotes{}
+	MapByAuthor = map[string]Quotes{}
+)
+
+type Quotes struct {
+	ID     int64
+	Author string `json: "author"`
+	Quote  string `json: "quote"`
+}
